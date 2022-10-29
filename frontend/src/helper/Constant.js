5f85892a4f2f3ac414e383b21e1aa2aae8c45390
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-29 17:54:37 +0800

    User Details Integrated with Backend

--- frontend/src/helper/Constant.js
+++ frontend/src/helper/Constant.js
@@ -1,13 +1,14 @@
 export const BASE_URL = "http://localhost:8081";
 
 export const PEOPLE_ENDPOINTS = {
+  GetAll: "/dev/v1/people/listall",
+  RefreshToken: "/dev/v1/people/getinfo",
   Login: "/dev/v1/people/login",
   Logout: "/dev/v1/people/logout",
   Register: "/dev/v1/people/register",
   RegisterCollector: "/dev/v1/people/register/collector",
   RegisterAdmin: "/dev/v1/people/register/admin",
-  RefreshToken: "/dev/v1/people/getinfo",
-  GetAll: "/dev/v1/people/listall",
+  UpdateDetails: "/dev/v1/people/update",
 }
 
 export const ROLES = {
