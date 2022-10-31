1fb2a11c7d6cc561ad0c48218d3999331e6c303e
Liu Fang <e0843571@u.nus.edu>
2022-10-31 18:28:48 +0800

    Add comment block - 31 Oct

--- backend/src/main/java/com/backend/configuration/CustomException.java
+++ backend/src/main/java/com/backend/configuration/CustomException.java
@@ -1,5 +1,11 @@
 package com.backend.configuration;
 
+//Purpose:
+//    - Create custom exception
+//
+//Author:
+//    - Xu Hong Lew
+
 public class CustomException extends Exception {
     public CustomException(String message) {
         super(message);
