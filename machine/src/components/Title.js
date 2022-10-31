a8e74ae1ca811019c5d5bdfcc8b026a7b272da17
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-31 13:14:51 +0800

    Added Title Component for Consistency

---
+++ machine/src/components/Title.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ROLES } from "../helper/Constant";
+import useAuth from "../hooks/useAuth";
+import useLogout from "../hooks/useLogout";
+
+const Title = () => {
+  const { auth } = useAuth();
+  const navigate = useNavigate();
+  const logout = useLogout();
+  const signOut = async () => {
+    await logout();
+    navigate("/login", {
+      state: { message: "You have logged out successfully" },
+    });
+  };
+
+  return (
+    <>
+      <section className="service-wrapper py-3">
+        <Link onClick={signOut}>
+          <span className="h2 text-center col-12 py-5 text-dark h4">BATTERY</span>{" "}
+          <span className="h2 text-center col-12 py-5 text-primary h4">L!fe</span>
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default Title;
