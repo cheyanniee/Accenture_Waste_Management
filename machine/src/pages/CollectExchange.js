eb854958debbdd4e2cc59e042f24733323ab2311
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-11-01 00:04:16 +0800

    Collect Exchange Completed

--- machine/src/pages/CollectExchange.js
+++ machine/src/pages/CollectExchange.js
@@ -7,41 +7,65 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Title from "../components/Title";
 
+/*
+    Purpose:
+        - Allow users to collect batteries
+        - Close tray upon completion
+
+    Restriction:
+        - Only those with ROLES.User will be able to access this page.
+
+    Endpoints:
+        - NIL
+
+    Author:
+        - Cheyanne Lim
+*/
+
 const CollectExchange = () => {
-  const { auth } = useAuth();
-  const navigate = useNavigate();
+    const { auth } = useAuth();
+    const navigate = useNavigate();
 
-  const done = () => {
-    console.log("Done Retrieving New Batteries");
-    navigate("/logout", { replace: true });
-  }
+    const done = () => {
+        console.log("Done Retrieving New Batteries");
+        navigate("/logout", { replace: true });
+    }
 
-  return (
-    <>
-      <Header />
-      <Title />
+    return (
+        <>
+            <Header />
+            <Title />
 
-      <section className="container-fluid pb-3">
+            <section className="container py-5">
+                <div className="row justify-content-center">
+                    <div className="col-lg-4 border px-3 rounded-3">
+                        <div className="row">
+                            <h1 className="col-12 col-xl-8 h2 text-left text-primary pt-3 mb-3">
+                                Hi {auth?.firstName}!
+                            </h1>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <h3>
+                                Please Collect Your Batteries!
+                            </h3>
+                        </div>
 
-          <div className="row">
-            <h3>
-              Please Collect Your Batteries!
-            </h3>
-          </div>
+                        <div className="h2 py-3">
+                            <button
+                                className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
+                                onClick={done}
+                            >
+                                Done Retrieving Batteries
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-          <div className="h2 py-3">
-            <button
-              className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
-              onClick={done}
-            >
-              Done Retrieving Batteries
-            </button>
-          </div>
-      </section>
-
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 };
 
 export default CollectExchange;
