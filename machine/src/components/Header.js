9ccd3cc7df2298e102d78fd6ef8416f4a42abadc
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-31 13:32:23 +0800

    Added Title Block. Removed Redundancies.

--- machine/src/components/Header.js
+++ machine/src/components/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { ROLES } from "../helper/Constant";
-import useAuth from "../hooks/useAuth";
 import useLogout from "../hooks/useLogout";
 
+/*
+    Purpose:
+        - Allow users to easily access pages.
+
+    Restriction:
+        - Only appropriate pages will be displayed to each role.
+
+    Endpoints:
+        - NIL
+
+    Author:
+        - Cheyanne Lim
+*/
+
 const Header = () => {
-  const { auth } = useAuth();
   const navigate = useNavigate();
   const logout = useLogout();
   const signOut = async () => {
@@ -16,22 +27,24 @@ const Header = () => {
   };
 
   return (
-    <nav
-      id="main_nav"
-      className="navbar navbar-expand-lg navbar-light bg-white shadow"
-    >
-      <div className="container d-flex justify-content-between align-items-center">
-        <Link className="navbar-brand h1" to="/">
-          <i className="bx bx-buildings bx-sm text-dark"></i>
-          <span className="text-dark h4">BATTERY</span>{" "}
-          <span className="text-primary h4">L!fe</span>
-        </Link>
+    <>
+      <nav
+        id="main_nav"
+        className="navbar navbar-expand-lg navbar-light bg-white shadow"
+      >
+        <div className="container d-flex justify-content-between align-items-center">
+          <Link className="navbar-brand h1" onClick={signOut}>
+            <i className="bx bx-buildings bx-sm text-dark"></i>
+            <span className="text-dark h4">BATTERY</span>{" "}
+            <span className="text-primary h4">L!fe</span>
+          </Link>
 
-        <Link className="nav-link" onClick={signOut}>
-          <i className="bx bx-log-out bx-sm text-primary"></i>
-        </Link>
-      </div>
-    </nav>
+          <Link className="nav-link" onClick={signOut}>
+            <i className="bx bx-log-out bx-sm text-primary"></i>
+          </Link>
+        </div>
+      </nav>
+    </>
   );
 };
 
