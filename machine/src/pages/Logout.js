377f09520766586567621f123fb5b3db7ba04a7f
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-31 17:30:07 +0800

    Logout Complete

--- machine/src/pages/Logout.js
+++ machine/src/pages/Logout.js
@@ -22,41 +22,50 @@ import Title from "../components/Title";
 */
 
 const Logout = () => {
-  const navigate = useNavigate();
-  const logout = useLogout();
-  const signOut = async () => {
-    await logout();
-    navigate("/login", {
-      state: { message: "You have logged out successfully" },
-    });
-  };
+    const navigate = useNavigate();
+    const logout = useLogout();
+    const signOut = async () => {
+        await logout();
+        navigate("/login", {
+            state: { message: "You have logged out successfully" },
+        });
+    };
 
-  return (
-    <>
-      <Header />
-      <Title />
+    return (
+        <>
+            <Header />
+            <Title />
 
-      <section className="container-fluid pb-3">
+            <section className="container py-5">
+                <div className="row justify-content-center">
+                    <div className="col-lg-4 border px-3 rounded-3">
+                        <div className="row">
+                            <h1 className="col-12 col-xl-8 h2 text-left text-primary pt-3 mb-3">
+                                Have a good day!
+                            </h1>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <h3>
+                                Please logout:
+                            </h3>
+                        </div>
 
-          <div className="row">
-            <h3>
-              Thank you! Please Log Out!
-            </h3>
-          </div>
+                        <div className="h2 py-3">
+                            <button
+                                className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
+                                onClick={signOut}
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-          <div className="h2 py-3">
-            <button
-              className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
-              onClick={signOut}
-            >
-              Logout
-            </button>
-          </div>
-      </section>
-
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 };
 
 export default Logout;
