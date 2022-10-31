68ede1820029d2af06882167a659013f0b31a116
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-31 18:38:16 +0800

    Collector Action Done

--- machine/src/pages/CollectorAction.js
+++ machine/src/pages/CollectorAction.js
@@ -5,49 +5,66 @@ import useAuth from "../hooks/useAuth";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Login from "../components/Login";
+import Title from "../components/Title";
+
+/*
+    Purpose:
+        - Collector to open machine by pressing start.
+
+    Restriction:
+        - Only those with ROLES.Collector will be able to access this page.
+
+    Endpoints:
+        - NIL
+
+    Author:
+        - Cheyanne Lim
+*/
 
 const CollectorAction = () => {
-  const { auth } = useAuth();
-  const navigate = useNavigate();
+    const { auth } = useAuth();
+    const navigate = useNavigate();
 
-  const done = () => {
-    console.log("Done Collecting Batteries");
-    navigate("/confirmCollection", { replace: true });
-  }
+    const done = () => {
+        console.log("Done Collecting Batteries");
+        navigate("/confirmCollection", { replace: true });
+    }
 
-  return (
-    <>
-      <Header />
+    return (
+        <>
+            <Header />
+            <Title />
 
-      <section className="service-wrapper py-3">
-        <div className="container-fluid pb-3">
-          <div className="row">
-            <h2 className="h2 text-center col-12 py-5 semi-bold-600">
-              Battery Life
-            </h2>
-          </div>
+            <section className="container py-5">
+                <div className="row justify-content-center">
+                    <div className="col-lg-4 border px-3 rounded-3">
+                        <div className="row">
+                            <h1 className="col-12 col-xl-8 h2 text-left text-primary pt-3 mb-3">
+                                Hi {auth?.firstName}!
+                            </h1>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <h3>
+                                Please Click to Begin Collection!
+                            </h3>
+                        </div>
 
-          <div className="row">
-            <h3>
-              Please Click to Begin Collection!
-            </h3>
-          </div>
+                        <div className="h2 py-3">
+                            <button
+                                className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
+                                onClick={done}
+                            >
+                                Start Collection
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-          <div className="h2 py-3">
-            <button
-              className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
-              onClick={done}
-            >
-              Start Collection
-            </button>
-          </div>
-        </div>
-      </section>
-
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 };
 
 export default CollectorAction;
