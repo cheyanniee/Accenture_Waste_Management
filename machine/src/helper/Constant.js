211512849ba804a178e7df59f7d85fe4b5e695c3
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-11-01 00:06:14 +0800

    Cleanup

--- machine/src/helper/Constant.js
+++ machine/src/helper/Constant.js
@@ -16,49 +16,40 @@
 export const BASE_URL = "https://localhost";
 
 export const PEOPLE_ENDPOINTS = {
-  RefreshToken: "/dev/v1/people/getinfo",
-  Login: "/dev/v1/people/login",
-  Logout: "/dev/v1/people/logout",
+    RefreshToken: "/dev/v1/people/getinfo",
+    Login: "/dev/v1/people/login",
+    Logout: "/dev/v1/people/logout",
 }
 
 export const TRANSACTION_ENDPOINTS = {
-  StartR: "/dev/v1/transaction/create/start-r",
-  StartE: "/dev/v1/transaction/create/start-e",
-  Confirm: "/dev/v1/transaction/create/yes",
-  Reject: "/dev/v1/transaction/create/no",
-  GetByID: "/dev/v1/transaction/find",
+    StartR: "/dev/v1/transaction/create/start-r",
+    StartE: "/dev/v1/transaction/create/start-e",
+    Confirm: "/dev/v1/transaction/create/yes",
+    Reject: "/dev/v1/transaction/create/no",
+    GetByID: "/dev/v1/transaction/find",
 }
 
 export const TRANSACTION_ENTRY_ENDPOINTS = {
-  Create: "/dev/v1/transactionentry/create",
-  GetByID: "/dev/v1/transactionentry/find",
+    Create: "/dev/v1/transactionentry/create",
+    GetByID: "/dev/v1/transactionentry/find",
 }
 
 export const MACHINE_ENDPOINTS = {
-  GetByID: "/dev/v1/machine/get/",
-  UpdateCurrentLoad: "/dev/v1/machine/update/currentload",
-}
-
-export const BATTERY_ENDPOINTS = {
-  GetAll: "/dev/v1/battery/listall",
+    GetByID: "/dev/v1/machine/get/",
+    UpdateCurrentLoad: "/dev/v1/machine/update/currentload",
 }
 
 export const TASK_ENDPOINTS = {
-  GetAll: "/dev/v1/task/listall",
-  GetByID: "/dev/v1/task/collector",
-  Collected: "/dev/v1/task/machine/collected/",
-  GetByMachine: "/dev/v1/task/machine/",
-}
-
-export const LOCATION_ENDPOINTS = {
-  GetAll: "/dev/v1/location/listall",
-  GetDistrict: "/dev/v1/location/getdistrict?postcode=",
+    GetAll: "/dev/v1/task/listall",
+    GetByID: "/dev/v1/task/collector",
+    Collected: "/dev/v1/task/machine/collected/",
+    GetByMachine: "/dev/v1/task/machine/",
 }
 
 export const ROLES = {
-  User: "user",
-  Collector: "collector",
-  Admin: "admin",
+    User: "user",
+    Collector: "collector",
+    Admin: "admin",
 };
 
 export const ACTION_TYPES = {
@@ -66,4 +57,7 @@ export const ACTION_TYPES = {
     Recycle: "recycle",
 }
 
-export const BATTERY_EXCHANGE_TYPES = ["AA", "AAA"];
+export const BATTERY_EXCHANGE_TYPES = [
+    "AA",
+    "AAA"
+];
