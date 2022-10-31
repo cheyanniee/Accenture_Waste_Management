728c36d2c4716181e027eb157545b399f8fa96cb
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-31 11:58:46 +0800

    Updated For SSL

--- machine/src/helper/Constant.js
+++ machine/src/helper/Constant.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8080";
-//export const BASE_URL = "https://localhost";
+//export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://localhost";
 
 export const PEOPLE_ENDPOINTS = {
   GetAll: "/dev/v1/people/listall",
