643e946c948be9b65754ca6ef1c058cc69d15f8c
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-31 13:35:24 +0800

    Added Title Block. Removed Redundancies.

--- machine/src/pages/Logout.js
+++ machine/src/pages/Logout.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import useAuth from "../hooks/useAuth";
 import useLogout from "../hooks/useLogout";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Title from "../components/Title";
+
+/*
+    Purpose:
+        - Logout after all actions are completed
+
+    Restriction:
+        - NIL
+
+    Endpoints:
+        - NIL
+
+    Author:
+        - Cheyanne Lim
+*/
 
 const Logout = () => {
-  const { auth } = useAuth();
   const navigate = useNavigate();
   const logout = useLogout();
   const signOut = async () => {
@@ -21,14 +34,9 @@ const Logout = () => {
   return (
     <>
       <Header />
+      <Title />
 
-      <section className="service-wrapper py-3">
-        <div className="container-fluid pb-3">
-          <div className="row">
-            <h2 className="h2 text-center col-12 py-5 semi-bold-600">
-              Battery Life
-            </h2>
-          </div>
+      <section className="container-fluid pb-3">
 
           <div className="row">
             <h3>
@@ -44,7 +52,6 @@ const Logout = () => {
               Logout
             </button>
           </div>
-        </div>
       </section>
 
       <Footer />
