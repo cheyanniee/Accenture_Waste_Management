9610103a79ae876f6de9cc59f628c797f2d75457
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-31 14:49:30 +0800

    Added Title Block. Removed Redundancies. Added Fields for Vending Machine.

--- machine/src/context/AuthProvider.js
+++ machine/src/context/AuthProvider.js
@@ -1,15 +1,44 @@
 import { createContext, useState } from "react";
 
+/*
+    Purpose:
+        - Store user details when logged in to use across pages
+
+    Restriction:
+        - NIL
+
+    Endpoints:
+        - NIL
+
+    Author:
+        - Alex Lim & Cheyanne Lim
+*/
+
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({});
-  console.log("Auth Provider:--", auth?.token);
-  return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    const [auth, setAuth] = useState({});
+    const [transaction, setTransaction] = useState({});
+    const [machineID, setMachineID] = useState({});
+
+    machineID = 14;
+
+    /* console.log("Auth Provider:--", auth?.token); */
+
+    return (
+        <AuthContext.Provider
+            value={{
+                auth,
+                setAuth,
+                transaction,
+                setTransaction,
+                machineID,
+                setMachineID
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthContext;
