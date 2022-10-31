08791e0af4d755d6e44e0c6be5e201614fa961c0
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-31 13:15:40 +0800

    Allow Users to Choose Action. Added Title Block. Redesigned UI

--- machine/src/pages/UserAction.js
+++ machine/src/pages/UserAction.js
@@ -1,3 +1,17 @@
+{/*
+    Purpose:
+        Allow user to choose an action after logging in.
+
+    Restriction:
+        Only those with ROLES.User will be able to access this page.
+
+    Endpoints:
+        NIL
+
+    Author:
+        Cheyanne Lim
+*/}
+
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -5,61 +19,63 @@ import useAuth from "../hooks/useAuth";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Title from "../components/Title";
 
 const UserAction = () => {
-  const { auth } = useAuth();
-  const navigate = useNavigate();
+    const { auth } = useAuth();
+    const navigate = useNavigate();
 
-  const recycle = () => {
-    console.log("Recycle");
-    navigate("/insertRecycling", { replace: true });
-  }
+    const recycle = () => {
+        navigate("/insertRecycling", { replace: true });
+    }
 
-  const exchange = () => {
-    console.log("Exchange");
-    navigate("/inputExchange", { replace: true });
-  }
+    const exchange = () => {
+        navigate("/inputExchange", { replace: true });
+    }
 
-  return (
-    <>
-      <Header />
+    return (
+        <>
+            <Header />
+            <Title />
 
-      <section className="service-wrapper py-3">
-        <div className="container-fluid pb-3">
-          <div className="row">
-            <h2 className="h2 text-center col-12 py-5 semi-bold-600">
-              Battery Life
-            </h2>
-          </div>
+            <section className="container py-5">
+                <div className="row justify-content-center">
+                    <div className="col-lg-4 border px-3 rounded-3">
+                        <div className="row">
+                            <h1 className="col-12 col-xl-8 h2 text-left text-primary pt-3 mb-3">
+                                Welcome Back {auth?.firstName}!
+                            </h1>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <h3>
+                                Please select an option:
+                            </h3>
+                        </div>
 
-          <div className="row">
-          <h3>
-            Welcome Back {auth?.firstName}!
-          </h3>
-          </div>
 
+                        <div className="h2 py-3">
+                            <button
+                                className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
+                                onClick={recycle}
+                            >
+                                Recycle Old Batteries
+                            </button>
+                            <br />
+                            <button
+                                className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
+                                onClick={exchange}
+                            >
+                                Exchange Points for New Batteries
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-          <div className="h2 py-3">
-          <button
-              className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
-              onClick={recycle}
-            >
-              Recycle Old Batteries
-            </button>
-            <br />
-            <button
-              className="btn btn-secondary rounded-pill px-md-5 px-4 py-2 radius-0 text-light light-300"
-              onClick={exchange}
-            >
-              Exchange Points for New Batteries
-            </button>
-        </div>
-        </div>
-      </section>
-
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 };
 
 export default UserAction;
